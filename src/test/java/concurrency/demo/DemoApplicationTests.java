0843e0be83fc5341a75c0df37513433b1acc1867
leegwichan <cnddkscndgus@naver.com>
2025-05-27 15:11:26 +0900

    init : 프로젝트 초기 설정

---
+++ src/test/java/concurrency/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package concurrency.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
