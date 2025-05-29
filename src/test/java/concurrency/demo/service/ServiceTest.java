e0fd6892350b7b2536982e0e2fb4dcb8fcda1ba2
leegwichan <cnddkscndgus@naver.com>
2025-05-29 17:48:59 +0900

    feat : 동시성 테스트 시, 해당 작업이 걸린 시간을 측정

--- src/test/java/concurrency/demo/service/ServiceTest.java
+++ src/test/java/concurrency/demo/service/ServiceTest.java
@@ -6,12 +6,16 @@ import concurrency.demo.repository.MemberRepository;
 import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @ExtendWith(DatabaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public abstract class ServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceTest.class);
 
     @Autowired
     protected MemberRepository memberRepository;
@@ -28,10 +32,13 @@ public abstract class ServiceTest {
                 .mapToObj(i -> new Thread(task))
                 .toList();
 
+        long startTime = System.currentTimeMillis();
         threads.forEach(Thread::start);
         for (Thread thread : threads) {
             thread.join();
         }
+        long endTime = System.currentTimeMillis();
+        log.info("All jobs(threads) finished in {} ms", endTime - startTime);
     }
 }
 
